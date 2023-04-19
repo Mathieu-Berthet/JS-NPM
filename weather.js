0dd84e068585e948e106dc1c4bc16f3b622363ef
Mathieu <berthetmathieu@free.fr>
2023-04-19 10:33:10 +0200

    temperature et etat du ciel a SF

--- weather.js
+++ weather.js
@@ -3,11 +3,12 @@ console.log("Weather");
 
 let weath = require('weather-js');
 
+let myWeath;
+
 weath.find({search:'San Francisco, CA', degreetype: 'F'}, function(error, result) {
     if(error)
     {
         console.log(error);
     }
-
-    console.log(JSON.stringify(result, null, 2));
-})
+    console.log("Température : ", result[0].current['temperature'] + "\nEtat du ciel : " + result[0].current['skytext']);
+});
